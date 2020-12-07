da9ee18d7691b44136a34a6783eed9aa3200942f
semeraro <semeraro@tacc.utexas.edu>
2020-12-07 16:43:01 -0600

    test code for image in pythonimagetest notebook

--- txr/render/image.py
+++ txr/render/image.py
@@ -16,12 +16,30 @@ class image:
         self._data = np.zeros((height,width,3),dtype=np.uint8)
     
     def __getitem__(self,key):
-        return self._data[key]
+        print(f'length of input tuple {len(key)}')
+        newkey = self._reverse_height_index(key)
+        print(f'{newkey}')
+        return self._data[newkey,:]
+        #return self._data[key]
     def __setitem__(self,key,value):
-        self._data[key] = value
+        self._data[self._height - key[0] -1,key[1]] = value
     @property
     def width(self):
         return self._width
     @property
     def height(self):
         return self._height
+    def _reverse_height_index(self,key):
+        # this reverses the height index value
+        print(key[0].step)
+        print(('no','yes')[key[0].step is None])
+        if isinstance(key[0],slice): # got a slice
+            key0 = slice(self._height - key[0].start -1,\
+                             self._height - key[0].stop  -1,\
+                              (-(key[0].step),None)[key[0].step is None])
+        else:
+            key0 = self._height - key[0] - 1
+        if len(key) < 3:
+            return (key0,key[1])
+        else:
+            return (key0,key[1],key[2])
