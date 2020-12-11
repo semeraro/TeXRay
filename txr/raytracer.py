4dfc26a2975ca7e668b2cef85d0fa6cfc968bac9
semeraro <semeraro@tacc.utexas.edu>
2020-12-11 17:20:16 -0600

    Began raytracer code to test bits of ray and image modules. cleaned prototyping notebook

---
+++ txr/raytracer.py
@@ -0,0 +1,68 @@
+# experiment with the txr package. Build a raytracer.
+# import the ray class
+import sys 
+sys.path.append("c:\\Users\\Dave Semeraro\\Documents\\TeXRay")
+from txr.render import rays, image
+import matplotlib.pyplot as plt
+from PIL import Image
+#%matplotlib inline
+import numpy as np
+#
+# Going to need an image
+#
+height = 100
+width = 200
+image.image(height,width)
+#
+# now we are going to need some rays
+# we are going to need a list of origins and directions
+#
+origins = []
+directions = []
+#
+# set our view point to be the origin of the global coordinates
+# set the up direction to be positive y, right to be positive x
+# and set the view direction to be the negative z direction. 
+# 
+up = np.array((0.,1.,0.),dtype=np.float)
+eye = np.array((0.,0.,0.),dtype=np.float)
+#
+# now we need a viewport which we place at z = -1
+# 
+lower_left = np.array((-2.0,-1.0,-1.0))
+# direction vectors left-right down-up
+horiz = np.array((4.0,0.,0.))
+vert = np.array((0.,2.,0.))
+# set the origins and directions so we can use them to generate ray data
+for i in range(height):
+    for j in range(width):
+        origins.append(eye)
+        u = float(j)/float(width)
+        v = float(i)/float(height)
+        direction = lower_left + u*horiz +v*vert 
+        directions.append(direction)
+raygen = rays.ray_generator(origins,directions)
+# use the ray generator to make rays
+raygen.generate()
+# get the ray group container from the generator
+rayGroup = raygen.ray_group
+print(f'ray group has {rayGroup.numrays} rays')
+#
+# How about a ray intersection function.
+#
+def hit_sphere(r,center,radius):
+    oc = r.origin - center
+    a = np.dot(r.direction , r.direction)
+    b = 2.0*np.dot(oc,r.direction)
+    c = np.dot(oc,oc) - radius*radius
+    disc = b*b -4.0*a*c
+    return (disc > 0)
+#
+def color(r):
+    center = np.array((0.,0.,-1.))
+    radius = 0.5
+    if hit_sphere(r,center,radius):
+        return((255,0,0))
+    else:
+        return((0,0,255))
+#
